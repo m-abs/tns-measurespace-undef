29478d5443775a5a592e12a5a38019e55bf2f58c
Morten Sjøgren <m_abs@mabs.dk>
2019-05-13 00:28:09 +0200

    some useless commit message

--- src/app/home/sub1/sub1.component.ts
+++ src/app/home/sub1/sub1.component.ts
@@ -1,17 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { EventData } from "tns-core-modules/ui/core/view/view";
 
 @Component({
-    selector: "ns-sub1",
+    selector: "ns-sub1, StackLayout[ns-sub1]",
     templateUrl: "./sub1.component.html",
     styleUrls: ["./sub1.component.scss"],
     moduleId: module.id
 })
-export class Sub1Component implements OnInit {
-    constructor() {}
-
-    ngOnInit() {}
-
+export class Sub1Component {
     public onTap(event: EventData) {
         console.log(`Sub1Component.onTap(${event.object})`);
     }
--- src/app/home/sub1/sub2/sub2.component.ts
+++ src/app/home/sub1/sub2/sub2.component.ts
@@ -1,13 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
-    selector: "ns-sub2",
+    selector: "ns-sub2, StackLayout[ns-sub2]",
     templateUrl: "./sub2.component.html",
     styleUrls: ["./sub2.component.scss"],
     moduleId: module.id
 })
-export class Sub2Component implements OnInit {
-    constructor() {}
-
-    ngOnInit() {}
+export class Sub2Component {
+    public showChild = false;
 }
